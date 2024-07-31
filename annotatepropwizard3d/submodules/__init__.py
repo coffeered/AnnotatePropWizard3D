05db80988ecddd18760024e14c3ba6ac3e6b9c90
Jim <coffeered.yu@gmail.com>
2024-07-31 11:33:20 +0800

    build: beta 4

--- annotatepropwizard3d/submodules/__init__.py
+++
@@ -1,8 +0,0 @@
-from annotatepropwizard3d.submodules.Cutie import cutie, gui
-from annotatepropwizard3d.submodules.SAM_Med2D import segment_anything
-
-__all__ = [
-    "cutie",
-    "gui",
-    "segment_anything",
-]
